5093c5deb843d931d5444824850687e7dffdb286
Joe <pyaephyokyaw.work@gmail.com>
2024-07-05 00:22:11 +0700

    fix: download button text

--- src/app/components/Download.tsx
+++ src/app/components/Download.tsx
@@ -24,7 +24,7 @@ export default function Download() {
         onClick={() => handleSubmit()}
         className="flex w-full justify-center rounded-md bg-amber-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-amber-500-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-amber-600"
       >
-        Download
+        Download CSV
       </button>
     </div>
   );
